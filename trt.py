33dcaf45627f85fce27a37e1ac95e1a96ff2fc3c
IKB <113422420+IKBxIK@users.noreply.github.com>
2023-02-26 16:09:11 +0500

    Update trt.py

--- trt.py
+++ trt.py
@@ -4,5 +4,16 @@ os.system('git pull')
 trt=platform.architecture()[0]
 if trt=="32bit":
     print('Sorry 32 Bit Not Supported...')
+
+import os, sys, platform,time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://youtube.com/@ikbsolution2665')
+
+trt=platform.architecture()[0]
+if trt=="32bit":
+    print('Sorry 32 Bit Not Supported...')
 elif trt=="64bit":
     __import__("trt1")
